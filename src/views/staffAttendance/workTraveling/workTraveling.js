911a712e9b935597998b3fd7747909aa0dfe6122
jiangyx <2055108207@qq.com>
2020-09-01 09:34:59 +0800

    离职管理

--- src/views/staffAttendance/workTraveling/workTraveling.js
+++ src/views/staffAttendance/workTraveling/workTraveling.js
@@ -1,100 +1,19 @@
 
-import moment from 'moment'
-import TableFilter from '@/components/TableFilter/index.vue'
-import { STable, Ellipsis } from '@/components'
-import { getServiceList } from '@/api/user'
-import {columns,filterList,result} from './codeList.js'
-
-const statusMap = {
-  0: {
-    status: 'default',
-    text: '关闭'
-  },
-  1: {
-    status: 'processing',
-    text: '运行中'
-  },
-  2: {
-    status: 'success',
-    text: '已上线'
-  },
-  3: {
-    status: 'error',
-    text: '异常'
-  }
-}
+import SearchTable from '@/components/SearchTable/searchTable.vue'
 
 export default {
-  name: 'workTravelingList',
+  name: 'workTraveling',
   title: '出差管理',
   components: {
-    STable,
-    Ellipsis,
-    TableFilter
+    SearchTable
   },
   data () {
-    this.columns = columns
-    this.filterList = filterList
     return {
-      // create model
-      visible: false,
-      confirmLoading: false,
-      mdl: null,
-      // 高级搜索 展开/关闭
-      advanced: false,
-      // 查询参数
-      queryParam: {},
-      // 加载数据方法 必须为 Promise 对象
-      loadData: parameter => {
-        const requestParameters = Object.assign({}, parameter, this.queryParam)
-        console.log('loadData request parameters:', requestParameters)
-        return getServiceList(requestParameters)
-          .then(res => {
-            // return res.result
-            return result
-          })
-      },
-      selectedRowKeys: [],
-      selectedRows: []
-    }
-  },
-  filters: {
-    statusFilter (type) {
-      return statusMap[type].text
-    },
-    statusTypeFilter (type) {
-      return statusMap[type].status
-    }
-  },
-  computed: {
-    rowSelection () {
-      return {
-        selectedRowKeys: this.selectedRowKeys,
-        onChange: this.onSelectChange
-      }
+      showBtnList:[]//按钮权限list
     }
   },
   methods: {
-    handleAdd (data,flag) {
-      this.$router.push({
-        name:'baseForm',
-        query:{
-          title:`出差${flag == 1 ? '新增' : flag == 2 ? '详情' : '修改'}`,
-          flag
-        }
-      })
-    },
-    handleEdit (record) {
-      this.visible = true
-      this.mdl = { ...record }
-    },
-    handleSub (record) {
-      
-    },   
-    onSelectChange (selectedRowKeys, selectedRows) {
-      this.selectedRowKeys = selectedRowKeys
-      this.selectedRows = selectedRows
-    }
+    
   }
 }
 
--- src/views/staffAttendance/workTraveling/workTraveling.vue
+++ src/views/staffAttendance/workTraveling/workTraveling.vue
@@ -2,52 +2,14 @@
   @import "./workTraveling.less";
 </style>
 <template>
-<div class="workTraveling">
-    <!--这是workTraveling列表页面-->
-    <a-card :bordered="false">
-      <table-filter :filterList='filterList'/>
-      <div class="table-operator">
-        <a-button type="primary" icon="plus" @click="handleAdd(null,1)">新建</a-button>
-        <a-dropdown v-action:edit v-if="selectedRowKeys.length > 0">
-          <a-menu slot="overlay">
-            <a-menu-item key="1"><a-icon type="delete" />删除</a-menu-item>
-          </a-menu>
-          <a-button style="margin-left: 8px">
-            批量操作 <a-icon type="down" />
-          </a-button>
-        </a-dropdown>
-      </div>
-
-      <s-table
-        ref="table"
-        rowKey="key"
-        :columns="columns"
-        :filterList="filterList"
-        :data="loadData"
-        :alert="true"
-        :rowSelection="rowSelection"
-        showPagination="auto">
-        <span slot="serial" slot-scope="text, record, index">
-          {{ index + 1 }}
-        </span>
-        <span slot="status" slot-scope="text">
-          <a-badge :status="text | statusTypeFilter" :text="text | statusFilter" />
-        </span>
-        <span slot="description" slot-scope="text">
-          <ellipsis :length="4" tooltip>{{ text }}</ellipsis>
-        </span>
-
-        <span slot="action" slot-scope="text, record">
-        <template>         
-          <a @click="handleAdd(record,2)">查看</a>
-          <a-divider type="vertical" />
-          <a @click="handleAdd(record,3)">修改</a>
-        </template>
-        </span>
-      </s-table>
-    </a-card>
+  <div class="workTraveling">
+      <search-table pageCode='trip'/>
   </div>
 </template>
+
+
+<script src="./staffList.js"></script>
+
 
 
 <script src="./workTraveling.js"></script>
