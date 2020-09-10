81493f7bd80efe4e6347a1c2d56c6248bd16963f
songtielei <songtielei@foxmail.com>
2020-09-10 17:57:48 +0800

    jenkins

--- jenkins-test.groovy
+++ jenkins-test.groovy
@@ -2,4 +2,4 @@ env.DEST_HOST = '10.122.42.161'
 env.DEST_PORT = '22'
 env.DEST_USERNAME = 'tele'
 env.DEST_PASSWORD = '6yhnJI(ol.'
-env.MODULE_DIR = "/home/service/web/hro-front-test"
+env.MODULE_DIR = "/home/tele/web/hro"
