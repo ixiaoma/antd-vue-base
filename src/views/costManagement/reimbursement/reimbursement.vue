0481428d361bad1fec3326cc201e3fe57476d85f
jiangyx <2055108207@qq.com>
2020-09-25 15:55:26 +0800

    报销

--- src/views/costManagement/reimbursement/reimbursement.vue
+++ src/views/costManagement/reimbursement/reimbursement.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="exampleList">
-      <!-- <search-table :pageCode='pageCode'/> -->
-      <search-table :definekey='definekey'/>
-  </div>
+  <a-card class="table-page-search-wrapper">
+      <!-- <search-table :pageCode='pageCode'/> -->    
+      <a-tabs default-active-key="0" @change="callback">
+        <a-tab-pane :key="index.toString()" :tab="item.name" v-for="(item,index) in reimbursementList">
+          <search-table :definekey='item.definekey'/>
+        </a-tab-pane>
+      </a-tabs>
+  </a-card>
 </template>
 
 <script>
@@ -11,11 +15,26 @@ export default {
   name:'reimbursement',
   data () {
     return {
-      pageCode:'reimbursement'
+      pageCode:'reimbursement',
+      reimbursementList:[
+        {definekey:'reimbursement_traffic',name:'市内交通'},
+        {definekey:'reimbursement_travel',name:'差旅'},
+        {definekey:'reimbursement_administration',name:'行政办公'},
+        {definekey:'reimbursement_League',name:'团建'},
+        {definekey:'reimbursement_birthday',name:'生日费'},
+        {definekey:'reimbursement_train',name:'培训/考试费'},
+        {definekey:'reimbursement_meeting',name:'会议费'},
+        {definekey:'reimbursement_other',name:'其他'},
+      ]
     }
   },
   components: {
     SearchTable
   },
+  methods:{
+    callback(key) {
+      console.log(key);
+    }
+  }
 }
 </script>
