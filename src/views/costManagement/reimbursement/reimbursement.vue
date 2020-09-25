47fb957045f2ab1440c7c09345144c4e5100ada5
jiangyx <2055108207@qq.com>
2020-09-25 16:33:08 +0800

    报销

--- src/views/costManagement/reimbursement/reimbursement.vue
+++ src/views/costManagement/reimbursement/reimbursement.vue
@@ -1,11 +1,23 @@
 <template>
   <a-card class="table-page-search-wrapper">
-      <!-- <search-table :pageCode='pageCode'/> -->    
-      <a-tabs default-active-key="0" @change="callback">
-        <a-tab-pane :key="index.toString()" :tab="item.name" v-for="(item,index) in reimbursementList">
-          <search-table :definekey='item.definekey'/>
-        </a-tab-pane>
-      </a-tabs>
+      <search-table :pageCode='pageCode'>
+        <a-dropdown v-if="buttonList.includes('addReimbursement')">
+          <a-menu slot="overlay" @click="handleAdd">
+            <a-menu-item :key="item.definekey" v-for="item in reimbursementList1">
+              {{item.name}}
+            </a-menu-item>
+          </a-menu>
+          <a-button type="primary" style="margin-right:10px"> 日常 <a-icon type="down" /> </a-button>
+        </a-dropdown>
+        <a-dropdown v-if="buttonList.includes('addReimbursement')">
+          <a-menu slot="overlay" @click="handleAdd">
+            <a-menu-item :key="item.definekey" v-for="item in reimbursementList2">
+              {{item.name}}
+            </a-menu-item>
+          </a-menu>
+          <a-button type="primary"> 非行政 <a-icon type="down" /> </a-button>
+        </a-dropdown>
+      </search-table>
   </a-card>
 </template>
 
@@ -16,25 +28,35 @@ export default {
   data () {
     return {
       pageCode:'reimbursement',
-      reimbursementList:[
+      reimbursementList1:[
         {definekey:'reimbursement_traffic',name:'市内交通'},
         {definekey:'reimbursement_travel',name:'差旅'},
         {definekey:'reimbursement_administration',name:'行政办公'},
+      ],
+      reimbursementList2:[
         {definekey:'reimbursement_League',name:'团建'},
         {definekey:'reimbursement_birthday',name:'生日费'},
         {definekey:'reimbursement_train',name:'培训/考试费'},
         {definekey:'reimbursement_meeting',name:'会议费'},
-        {definekey:'reimbursement_other',name:'其他'},
-      ]
+        {definekey:'reimbursement_other',name:'其他'}   
+      ],
+      buttonList:[]
     }
   },
   components: {
     SearchTable
   },
   methods:{
-    callback(key) {
-      console.log(key);
-    }
+    handleAdd ({key}) {
+          const title = `${this.$route.meta.title}新增`
+          this.$router.push({
+              name:'baseForm',
+              query:{title,flag:1,definekey:key}
+          })
+      },
+  },
+  created(){
+    this.buttonList = this.$route.meta.buttonList?this.$route.meta.buttonList:[]
   }
 }
 </script>
